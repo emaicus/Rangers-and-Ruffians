619453601bd96d1091179e1ea410d07c2d5ea01c
emaicus <emaicus@outlook.com>
2017-10-07 15:22:07 -0400

    Finished md creation.

--- tinkering.py
+++ tinkering.py
@@ -4,16 +4,41 @@ import sys
 def class_md_out(theFile):
   if theFile == "":
     sys.exit(0)
-  with open(sys.argv[1]) as class_file:    
+  with open(theFile) as class_file:    
     data = json.load(class_file)
-    outfile = open("races.md", 'w')
+    outfile = open("classes.md", 'w')
     standard_classes = data["standard_classes"]
-    for class in standard_classes:
+    
+    for rangers_class in standard_classes:
+      name = rangers_class["names"][0]
+      description = rangers_class["description"]
+      if 'standings' in rangers_class:
+        standings = rangers_class["standings"]
+      else:
+        standings = name + "s come in so many shapes, sizes, and belief sets that there is no set standing for them."
+      abilities = rangers_class["abilities"]
+      stats = rangers_class["stats"]
+      outfile.write("### " + name + "  \n")
+      print(name) 
+      outfile.write(description + "  \n")
+      print(description)
+      outfile.write("**Standings:** " + standings + "  \n")
+      print(standings)
+      outfile.write("##### Abilities: " + "  \n")
+      for ability in abilities:
+        outfile.write("  * " + ability + "  \n")
+        print(ability)
+      outfile.write("\n##### Stats:  " + "  \n")
+      for stat in stats:
+        outfile.write("  * " +  stat + ": " + stats[stat] + "  \n")
+        print(stat + " " + stats[stat])
+      outfile.write("   \n")
+    outfile.close()
       
 
 def main():
-  if len(sys.argv) < 2:
-    print("ERROR: include the name of the races.json file.")
+  if len(sys.argv) < 3:
+    print("ERROR: include the name of the races.json and classes.json file.")
     sys.exit(0)
   with open(sys.argv[1]) as data_file:    
     data = json.load(data_file)
@@ -40,6 +65,7 @@ def main():
         outfile.write("  * " +  stat + ": " + stats[stat] + "  \n")
         print(stat + " " + stats[stat])
       outfile.close()
+  class_md_out(sys.argv[2])
 
 if __name__ == "__main__":
     main()
