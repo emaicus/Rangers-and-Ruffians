8c03c688b86c19233c36d44cecb3d700e44410b8
emaicus <emaicus@outlook.com>
2019-04-02 14:54:45 -0400

    began adding v2 spells

---
+++ data/make_spell_template.py
@@ -0,0 +1,14 @@
+import yaml
+
+with open('spells.yml') as data_file:
+  spell_books = yaml.load(data_file)
+
+empty_spellbooks = dict()
+
+for spell_book, chapters in spell_books.items():
+  empty_spellbooks[spell_book] = dict()
+  for chapter, spells in chapters.items():
+    empty_spellbooks[spell_book][chapter] = dict()
+
+with open('empty_spellbook.yml', 'w') as outfile:
+    yaml.dump(empty_spellbooks, outfile)
