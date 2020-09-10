25665efc74cdcbfed1b197e6542a66555e1e1b65
Evan Maicus <emaicus@outlook.com>
2020-09-09 20:54:26 -0400

    more bard work

--- code/generate_helpful_poohbah_stats.py
+++ code/generate_helpful_poohbah_stats.py
@@ -190,22 +190,22 @@ def get_average_weapon_for_level(level):
 
 
 def get_average_magic_weapon_for_level(level):
-  # Levels 0-1
+  # Levels 0-1, Tier 0
   if level < 2:
     return rnr_weapon(2, 6, hit_modifier=0)
-  # Levels 2-4
+  # Levels 2-4, Tier 1
   elif level < 5:
     return rnr_weapon(4, 6, hit_modifier=1)
-  # Levels 5-7
+  # Levels 5-7, Tier 2
   elif level < 8:
     return rnr_weapon(6, 8, hit_modifier=1)
-  # Levels 8-9
+  # Levels 8-9, Tier 3
   elif level < 10:
     return rnr_weapon(8, 8, hit_modifier=2)
-  # Levels 10-12
+  # Levels 10-12, Tier 4
   elif level < 13:
     return rnr_weapon(9, 10, hit_modifier=2)
-  # Levels 13-15
+  # Levels 13-15, Tier 5
   else:
     return rnr_weapon(20, 6, hit_modifier=2)
 
