6c10286f6854482719716bd69678de1935fd96c0
Evan Maicus <emaicus@outlook.com>
2024-01-17 20:50:26 -0500

    wip

--- src/app/rendering/weapon-renderer/weapon-renderer.component.ts
+++ src/app/rendering/weapon-renderer/weapon-renderer.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Weapon } from '../weapon';
+import { Weapon } from '../../data_types/Weapon';
 import { CommonModule } from '@angular/common';
 import { NgIf } from '@angular/common';
 
