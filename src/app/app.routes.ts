4be50937d14a7e7af91fb2dddf9f662a92d6efbf
Evan Maicus <emaicus@outlook.com>
2024-02-02 23:30:21 -0500

    Remove character page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './webpages/home-page/home-page.component';
 import { WeaponPageComponent } from './webpages/weapon-page/weapon-page.component';
-import { CharacterPageComponent } from './webpages/character-page/character-page/character-page.component';
 import { MonsterPageComponent } from './webpages/monster-page/monster-page.component';
 import { RacePageComponent } from './webpages/race-page/race-page.component';
 import { BackgroundPageComponent } from './webpages/background-page/background-page.component';
