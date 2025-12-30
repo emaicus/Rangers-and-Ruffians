7178cc9c630043644382e66c646a8618617fb0e3
Evan Maicus <emaicus@outlook.com>
2025-12-30 12:39:02 -0500

    Finished upgrade to node 19

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,10 +17,6 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    }),
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
     })
 ]
 };
